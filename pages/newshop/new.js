710bfca54dbfbe283df84ef8a7ca7a5c7bf3a28a
yinghui tan <yinghuitan18@gmail.com>
2019-04-12 16:07:31 +0800

    plx commit right branch

--- pages/newshop/new.js
+++ pages/newshop/new.js
@@ -7,20 +7,66 @@ Page({
   onLoad: function () {
   },
 
-  bindSubmit: function (e) {
+  showPage(e) {
+    // console.log(data)
+    // const shop = data.shop;
+
+    wx.navigateTo({
+      url: `../shops/shops`
+    });
+  },
+  
+  bindSubmit: function (f) {
+    console.log(f.detail.value)
+    wx.setStorageSync('name', f.detail.value.name)
+    wx.setStorageSync('address', f.detail.value.address)
+    wx.setStorageSync('image', f.detail.value.image)
+    wx.setStorageSync('description', f.detail.value.description)
+    wx.setStorageSync('opening_hours', f.detail.value.opening_hours)
+    wx.setStorageSync('phone_number', f.detail.value.phone_number)
+
     let shop = {
-      email: e.detail.value.email,
-      password: e.detail.value.password
+      name: f.detail.value.name,
+      address: f.detail.value.address,
+      image: f.detail.value.image,
+      description: f.detail.value.description,
+      opening_hours: f.detail.value.opening_hours,
+      phone_number: f.detail.value.phone_number,
     }
+
+    console.log(shop)
+
+    let page = this;
 
     wx.request({
       url: `https://mighty-forest-60697.herokuapp.com/api/v1/shops`,
       method: 'POST',
+      header: {
+        'X-Customer-Token': wx.getStorageSync('token'),
+        'X-Customer-Email': wx.getStorageSync('email')
+      },
       data: { shop: shop },
       success: res => {
         console.log(res)
-      }
-    })
+     
+          wx.reLaunch({
+            url: '/pages/shops/shops',
+          })
+      },
+    }),
+
+    wx.showToast({
+      title: 'Sending...',
+      icon: 'loading',
+      duration: 1500
+    });
+
+    wx.switchTab({
+      url: '/pages/shops/shops'
+    });
+
+
+      
   }
 })
 
